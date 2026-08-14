1e6d6775aae6f5d1d69f0e5dabc5eb0c733a258e
ilkayse7989-bit <ilkayse7989@gmail.com>
2026-08-14 14:20:13 +0300

    Android projesi: app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.readbook.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.readbook.app"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0-test"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
